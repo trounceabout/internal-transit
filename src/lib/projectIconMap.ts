5b9f7268819ba130f39a9e5eee438ab9b8e21de0
Cody Keisler <codykeisler@gmail.com>
2026-07-12 15:20:05 -0400

    Removed unused icons from project icon map

--- src/lib/projectIconMap.ts
+++ src/lib/projectIconMap.ts
@@ -1,9 +1,6 @@
 import type { ComponentType, SVGProps } from "react";
 import { Bell as BellDotIcon } from "pixelarticons/react/Bell";
 import { Waves2 as SurfboardIcon } from "pixelarticons/react/Waves2";
-import { SettingsCog as DashboardSquare01Icon } from "pixelarticons/react/SettingsCog";
-import { Settings2 as Settings02Icon } from "pixelarticons/react/Settings2";
-import { Map as CompassIcon } from "pixelarticons/react/Map";
 import { Key as KeyIcon } from "pixelarticons/react/Key";
 import { CastleTower as CastleTowerIcon } from "pixelarticons/react/CastleTower";
 
@@ -17,9 +14,6 @@ export const projectIconMap: Record<
 > = {
   BellDotIcon,
   SurfboardIcon,
-  DashboardSquare01Icon,
-  Settings02Icon,
-  CompassIcon,
   KeyIcon,
   CastleTowerIcon,
 };
